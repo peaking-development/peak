d71203b4f50a63f20a241af5fefcbe1e482b29ee
CoderPuppy <cpup@cpup-laptop>
2014-10-01 16:07:15 -0400

    Fix the path for devfs this isn't used yet

--- test.lua
+++ test.lua
@@ -2,7 +2,7 @@ local kernel = require('./kernel')({
 	mounts = {
 		{{}, require('./craftfs')(fs)};
 		{{'proc'}, require('./procfs')()};
-		--{{'dev'}, require('devfs')()};
+		--{{'dev'}, require('./devfs')()};
 	};
 	initPath = {'boot', 'init.lua'};
 })
